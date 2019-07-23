924a9ee6c68f348ba2aedfacf16da3d4628510a7
Mikl <Kush@MBP-Mikl.kyiv.epam.com>
2019-07-23 14:21:24 +0300

    added conffile for instance

--- resources.tf
+++ resources.tf
@@ -1,8 +1,34 @@
-resource "aws_vpc" "environment-example two" {
+resource "aws_vpc" "environment-example-two" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true 
   enable_dns_support = true
-  tags {
-      Name = "terraform_aws_vps_example"
+  tags = {
+      Name = "terraform-aws-vps-example"
   }
 }
+
+resource "aws_subnet" "subnet1" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)}"
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "subnet2" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 2, 2)}"
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  availability_zone = "us-east-1c"
+}
+
+resource "aws_security_group" "subnetsecuritygroup" {
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+
+  ingress {
+    cidr_blocks = [
+      "${aws_vpc.environment-example-two.cidr_block}"
+    ]
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+      }
+}
+
